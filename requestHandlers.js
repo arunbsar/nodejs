5b684c04c34553bf20fdfcb8f7fc01430a9c0c21
arunbsar@gmail.com <arun@Aruns-MacBook-Pro.local>
2018-12-18 00:32:02 +0000

    Updated request handler to show hostname

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,8 @@
 
 function start(response) {
     console.log("Request handler 'start' was called.");
-
-    var body = 'Hello World Version 4' + \
-    ' ';
+    var os = require("os");
+    var body = 'Hello World Version 1, from: ' + os.hostname();
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
