e66edbda3fd2e3c180d787de7b0ab710b62e51d6
Arun Kumar <arunbsar@gmail.com>
2018-12-18 00:40:55 +0000

    Updated version

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,7 +2,7 @@
 function start(response) {
     console.log("Request handler 'start' was called.");
     var os = require("os");
-    var body = 'Hello World Version 1, from: ' + os.hostname();
+    var body = 'Hello World Version 1.2, from: ' + os.hostname();
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
